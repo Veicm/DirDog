ebb5a1a2dff2a7c6120d076ad307c7865646f6ee
Veicm <connormannes@gmx.de>
2025-12-13 19:54:24 +0100

    added ipc dummy

---
+++ HandlerDog/src/ipc/receiver.py
@@ -0,0 +1,11 @@
+def dummy_receiver() -> list[dict[str, str | int]]:
+    """
+    _ description _
+
+    Args:
+        variable (any): _ variable description _.
+
+    Returns:
+        int: _ value description _.
+    """
+    raise NotImplementedError("function 'dummy_receiver()' is not implemented yet.")
