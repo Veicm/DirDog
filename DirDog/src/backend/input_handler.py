b94ddb2abcd59cdcc37daf689b50b4bf82b4c963
Veicm <180028722+Veicm@users.noreply.github.com>
2026-01-11 12:03:24 +0100

    init_CLI_backend

---
+++ DirDog/src/backend/input_handler.py
@@ -0,0 +1,10 @@
+class InputHandler:
+    """
+    This function provides function to handle specific user inputs made in the CLI.
+
+    Attributes:
+        data_storage_path (str): The path to the persistence storage.
+    """
+
+    def __init__(self, data_storage_path: str) -> None:
+        self.data_storage_path: str = data_storage_path
