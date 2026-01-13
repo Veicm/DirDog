c4e194891ecf7daf4fc66805a5b9aa4719a5f86b
Horst507 <lucaneidhart@gmx.de>
2026-01-13 11:03:12 +0100

    helpers.py aktualisieren

--- SentinelDog/helpers.py
+++ SentinelDog/helpers.py
@@ -1,43 +1,57 @@
 import time
 from pathlib import Path
 import json
-import hashlib
-import threading
 from multiprocessing.connection import Client
-
-
-with open("./check_changes_config.json", "r", encoding="utf-8") as config_file:
+from py_essentials import hashing as hs
+import threading
+json_lock = threading.Lock()
+with open('SentinelDog/check_changes_config.json', 'r', encoding='utf-8') as config_file:
     data = json.load(config_file)
-try:
+try:     
     print("[+] Connection is prepared!")
     address = ("localhost", 6000)
     conn = Client(address, authkey=b"secret password")
-    print("[+] Connection succesfull!")
+    print("[+] Connection succesfull!")    
 except:
-    print("[!] Error: Connecetion failed!")
+     print("[!] Error: Connecetion failed!")
+
+def sha256_file(path):
+
+    hash = hs.fileChecksum(path, "sha256")
+    return  str(hash)
 
 
-def sha256_file(path, chunk_size=8192):
-    h = hashlib.sha256()
-    with open(path, "rb") as f:
-        for chunk in iter(lambda: f.read(chunk_size), b""):
-            h.update(chunk)
-    return h.hexdigest()
-
-
-def push_change_files_into_api(path, type_of_action, new_path=None):
+def push_change_files_into_api(path,type_of_action,new_path=None):
     global data
     global conn
     parent_dir = str(Path(path).parent)
     extension = str(Path(path).suffix)
     file_name = str(Path(path).stem)
-    # print("--------------------------------------------")
-    # print(data["path"])
-    # print("----")
-    # print(path)
 
-    # print("-------------------------------------------------")
-    if str(parent_dir) != str(data["path"]):
+    try:
+        new_name = str(Path(new_path).stem)+str(Path(new_path).suffix)
+        new_file_extension = str(Path(new_path).suffix)
+    except:
+
+        new_name = None
+        print("Error beim auswerten des neuen Pfades. (Normal bei 3/4 operationen)")
+    
+    
+    
+    print("--------------------------------------------")
+    print(data["path"])
+    print("----")
+    print(parent_dir)
+
+
+    print(str(Path(data["path"]).parent.resolve(strict=True)))
+    print("------")
+    print(Path(parent_dir).resolve(strict=True))
+    print(Path(data["path"]).resolve(strict=True))
+
+
+    print("-------------------------------------------------")
+    if Path(parent_dir).resolve(strict=True) !=Path(data["path"]).resolve(strict=True):
         print("Eintrag wurde übersprungen da die Datei in einem Unterordner liegt!")
         return
 
@@ -45,7 +59,7 @@ def push_change_files_into_api(path, type_of_action, new_path=None):
         hashed_file = None
     else:
         try:
-            hashed_file = str(sha256_file(new_path if new_path else path))
+            hashed_file = str(sha256_file(new_path if new_path else path))    
         except (FileNotFoundError, PermissionError):
             hashed_file = None
             print("Eintrag wurde übersprungen da ein Ordner betroffen war !")
@@ -59,10 +73,13 @@ def push_change_files_into_api(path, type_of_action, new_path=None):
         "parent_dir": parent_dir,
         "file_name": file_name,
         "file_extension": extension,
-        "new_path": new_path,
+        "new_name": new_name,
+        "new_file_extension":new_file_extension,
         "last_modified": int(time.time()),
         "SHA-256-Hash": hashed_file,
-        "action": type_of_action,
-    }
+        "action": type_of_action
 
+
+    }
     conn.send(changed_file)
+    
