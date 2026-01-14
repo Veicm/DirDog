3bbe1a1f19c58984390004c00f0fa202d674ef10
Veicm <180028722+Veicm@users.noreply.github.com>
2026-01-14 18:51:31 +0100

    Updates (#11)

---
+++ SentinelDog/check_changes.py
@@ -0,0 +1,23 @@
+from watchdog.observers import Observer
+import json
+import threading
+from Handler_class import Handler
+
+json_lock = threading.Lock()
+
+
+with open("./check_changes_config.json", "r", encoding="utf-8") as config_file:
+    data = json.load(config_file)
+
+
+observer = Observer()
+observer.schedule(Handler(), path=data["path"], recursive=True)
+observer.start()
+
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
