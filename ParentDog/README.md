4e10f7cd8258f6069ba5371afb000020745afdb9
Veicm <180028722+Veicm@users.noreply.github.com>
2026-02-09 16:55:19 +0100

    major chagen on the parent class

---
+++ ParentDog/README.md
@@ -0,0 +1,3 @@
+# ParentDog
+
+The sub-program, which is used to manage that HandlerDog and SentinelDog are working all the time.
---
+++ ParentDog/src/__main__.py
@@ -0,0 +1,10 @@
+from parent import Parent
+
+
+def main() -> None:
+    parent = Parent()
+    parent.watch()
+
+
+if __name__ == "__main__":
+    main()
